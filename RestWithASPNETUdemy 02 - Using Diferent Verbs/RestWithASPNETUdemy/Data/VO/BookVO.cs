9279707e16edad1a2d5e7c1b87de4ab05a918bbb
Fábio R. Candiá <fabio.candia@gmail.com>
2018-07-26 18:35:04 -0300

    Adicionado DataContract

--- RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Data/VO/BookVO.cs
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using MySqlX.XDevAPI.Relational;
 using RestWithASPNETUdemy.Model.Base;
@@ -10,12 +11,23 @@ using RestWithASPNETUdemy.Model.Base;
 namespace RestWithASPNETUdemy.Data.VO
 {
     // ReSharper disable once InconsistentNaming
+    //Contrato entre os atributos e a estrutura da tabela
+    [DataContract]
     public class BookVO 
     {
+        [DataMember(Order = 1, Name = "id")]
         public long? Id { get; set; }
+
+        [DataMember(Order = 3, Name = "title")]
         public string Title { get; set; }
+
+        [DataMember(Order = 2, Name = "author")]
         public string Author { get; set; }
+
+        [DataMember(Order = 4, Name = "price")]
         public decimal Price { get; set; }
+
+        [DataMember(Order = 5, Name = "launch_date")]
         public DateTime LaunchDate { get; set; }
     }
 }
--- RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Model.Base
 {
-    //Contrato entre os atributos e a estrutura da tabela
-    //[DataContract]
     public class BaseEntity
     {
         public long? Id { get; set; }
